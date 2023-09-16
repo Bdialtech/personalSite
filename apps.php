14bea049f0153846e3927a477ab6c57e0808ff34
BDialtech <brad@dialtech.com>
2023-09-15 23:20:08 -0400

    Added RimCalc to apps list Added table of apps to apps page Small CSS tweaks Added Assets directory, and background image for Rimcalc

--- apps.php
+++ apps.php
@@ -1,11 +1,34 @@
 <html lang="en">
     <head>
         <link rel="stylesheet" href="css/default.css">
+        <link rel="stylesheet" href="css/apps.css">
         <title>Brad Dial's Web Apps</title>
     </head>
     <body>
-        <?php include 'objects/siteHeader.html'; ?>
-        
-        <?php include 'objects/siteFooter.html'; ?>
+        <div class="page">
+            <div class="content">
+                <?php include 'objects/siteHeader.html'; ?>
+
+                <table class="appList">
+                    <tr>
+                        <th></th>
+                        <th>App</th>
+                        <th>Description</th>
+                        <th>Commissioner</th>
+                        <th>Initial Build Date</th>
+                    </tr>
+                    <tr>
+                        <td><button type="button" class="appLauncher" onclick='window.open("apps/rimcalc.html", "_blank");'>Launch</button></td>
+                        <td>RimCalc</td>
+                        <td>A simple weekly value tracker for RimWorld</td>
+                        <td>R. Nolan</td>
+                        <td>August 2023</td>
+                    </tr>
+                </table>
+
+            </div>
+
+            <?php include 'objects/siteFooter.html'; ?>
+        </div>
     </body>
 </html>
