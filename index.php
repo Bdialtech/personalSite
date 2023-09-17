f466ae6699939c240e75def202c1e3d5c5a13ace
BDialtech <brad@dialtech.com>
2023-09-17 00:45:02 -0400

    Added framework for more pages - AboutMe.php - AboutSite.php - Small fix to apps page CSS - Added templates dir to objects dir, populated with page template

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/default.css">
+        <link rel="stylesheet" href="/css/default.css">
         <title>Brad Dial - Budding Web Developer</title>
     </head>
     <body>
---
+++ objects/templates/page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="/css/default.css">
+        <title>Title</title>
+    </head>
+    <body>
+        <div class="page">
+            <div class="content">
+                <?php include 'objects/siteHeader.html'; ?>
+                
+            </div>
+
+            <?php include 'objects/siteFooter.html'; ?>
+        </div>
+    </body>
+</html>
