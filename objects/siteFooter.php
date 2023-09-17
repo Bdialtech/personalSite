576047f2fe1f76dae73f1dbd2652c1f4238eaf2a
BDialtech <brad@dialtech.com>
2023-09-17 08:08:49 -0400

    Added background texture

--- objects/siteFooter.php
+++ objects/siteFooter.php
@@ -3,5 +3,6 @@
     <?php
         echo "<p>This page was last modified on " . date("F d, Y h:i A", $lastModify) . "</p>";
     ?>
+    <p><a href="https://www.freepik.com/free-vector/abstract-geometric-pattern-background_1195853.htm#query=background%20pattern&position=8&from_view=keyword&track=ais">Background image by vilmosvarga</a> on Freepik</p>
 
 </footer>
