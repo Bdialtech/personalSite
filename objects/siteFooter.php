e8ee2d9a151eb16087a0e663624113d619ce0a68
BDialtech <brad@dialtech.com>
2023-09-17 07:51:47 -0400

    CSS tweaks and footer changes

--- objects/siteFooter.php
+++ objects/siteFooter.php
@@ -1,7 +1,5 @@
 <link rel="stylesheet" href="/css/footer.css">
 <footer class="footer">
-    <p>This is the footer</p>
-    <p>My website doesn't have any copyrighting or anything like that but the footer looks really professional, doesn't it?</p>
     <?php
         echo "<p>This page was last modified on " . date("F d, Y h:i A", $lastModify) . "</p>";
     ?>
