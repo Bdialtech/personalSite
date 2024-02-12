42b09cd802e915c8cf501ad03d9674f303b5abc3
BDialtech <brad@dialtech.com>
2024-02-12 14:15:57 -0500

    Updated page template to include more meta info

--- objects/templates/page.php
+++ objects/templates/page.php
@@ -3,14 +3,16 @@
     <head>
         <?php $lastModify = filemtime(__FILE__); ?>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Content description">
         <link rel="stylesheet" href="/css/default.css">
         <title>Title</title>
     </head>
     <body>
         <div class="page">
         <?php include $_SERVER['DOCUMENT_ROOT'].'/objects/siteHeader.php'; ?>
-            <div class="content">
-               
+
+            <div class="content">       
                 
             </div>
 
